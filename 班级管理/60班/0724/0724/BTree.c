2a6620286a3d94a63e56a9eeb5953bfb60322751
clairad <623131118@qq.com>
2019-07-26 17:23:44 +0800

    60班7月26日代码注释补充

--- 班级管理/60班/0724/0724/BTree.c
+++ 班级管理/60班/0724/0724/BTree.c
@@ -113,19 +113,26 @@ void BinaryTreeInOrderNonR(BTNode* root)
 	Stack st;
 	StackInit(&st, 100);
 
-	while (cur || !StackIsEmpty(&st))
+	while (cur || !StackIsEmpty(&st)) //当cur为空且栈为空时，循环跳出，代表树遍历完毕
 	{
-		for (; cur; cur = cur->_left)
+		for (; cur; cur = cur->_left) 
+		//1、把目前的根及其所有的左孩子压栈，直到找不到左孩子为止
+		//2、以目前的右孩子作为根，继续将他的左孩子们压栈
 		{
 			StackPush(&st, cur);
 		}
 
 		if (!StackIsEmpty(&st))
 		{
-			cur = StackFront(&st);
-			putchar(cur->_data);
+			cur = StackFront(&st); 
+			//1、左孩子遍历完毕后，第一个没有左孩子的节点就是中序的第一个输出
+			//2、如果右孩子为空，此时栈里将会是下一个要访问的节点
+			//   如果有右孩子，那么此时栈里将会是以那个右孩子为跟的子树中第一个没有左孩子的节点
+			putchar(cur->_data); 
+			//1、由于没有左孩子了，所以打印根
 			StackPop(&st);
-			cur = cur->_right;
+			cur = cur->_right; 
+			//1、左子树和根遍历结束后，遍历它的右子树
 		}
 	}
 	StackDestory(&st);
@@ -137,30 +144,32 @@ void BinaryTreePostOrderNonR(BTNode* root)
 	BTNode * cur = root;
 
 	Stack st;
-	int tag[32] = { 0 };
+	int tag[32] = { 0 }; //左孩子遍历标签
 
 	StackInit(&st, 100);
 
 	while (cur || !StackIsEmpty(&st))
 	{
-		for (; cur; cur = cur->_left)
+		for (; cur; cur = cur->_left) //类似中序，将左孩子入栈，cur为空时，代表上一个节点的右孩子为空，只有这种情况，才可能进入下面的while
 		{
-			StackPush(&st, cur);
-			tag[st.size] = 0;
+			StackPush(&st, cur); //push操作会导致size+1
+			tag[st.size] = 0; //由于入栈的是左孩子，所以这里的左孩子遍历标签置0
 		}
 
-		while (!StackIsEmpty(&st) && tag[st.size] == 1)
+		//只要上面的for执行过哪怕一次循环（右孩子不为空），这个while的后半部分条件都不可能满足
+		while (!StackIsEmpty(&st) && tag[st.size] == 1) //左孩子还没遍历完成时，不能进入打印
+		//所以，这里确保了只有左右子树都遍历完成时，才会进入这个while
 		{
 			cur = StackFront(&st);
-			putchar(cur->_data);
-			StackPop(&st);
-			cur = NULL;
+			putchar(cur->_data); //打印根
+			StackPop(&st); //pop操作会导致size-1
+			cur = NULL; //为了循环正常跳出
 		}
 
 		if (!StackIsEmpty(&st))
 		{
-			tag[st.size] = 1;
-			cur = StackFront(&st)->_right;
+			tag[st.size] = 1; //进入这里证明左子树遍历完毕，左子树标签置1
+			cur = StackFront(&st)->_right; //进入右子树继续遍历
 		}
 	}
 	StackDestory(&st);
